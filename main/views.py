a77be0739f3427d9f4d003f25c3af70041025d35
sorfeb <soros21febriano@gmail.com>
2023-09-11 13:04:26 +0700

    modified template html

--- main/views.py
+++ main/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name': 'Pak Bepe',
-        'class': 'PBP A'
+        'name': 'Ballpoint pen',
+        'amount ': '1',
+        'description': 'Writing staple.'
     }
 
     return render(request, "main.html", context)
