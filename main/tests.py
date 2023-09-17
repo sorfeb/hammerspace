eb9945a26f9bd5fbe894a8d2c1d71a163f036aa9
sorfeb <soros21febriano@gmail.com>
2023-09-18 01:35:39 +0700

    test

--- main/tests.py
+++ main/tests.py
@@ -3,19 +3,20 @@ from main.models import Product
 from django.urls import reverse
 
 class mainTest(TestCase):
-    def test_main_url_is_exist(self):
+    def test_main_url_is_exist(self): #checks if /main/ exists
         response = Client().get('/main/')
         self.assertEqual(response.status_code, 200)
 
-    def test_main_using_main_template(self):
+    def test_main_using_main_template(self): #checks /main/ with main.html template
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
     
-    def test_model_creation(self):
+    def test_model_creation(self): #Creates an instance of Product with test attributes
         my_instance = Product.objects.create(name="Test", amount="2", description = "this_is_a_test")
         self.assertEqual(my_instance.name, "Test")
     
-    def test_show_main_view(self):
-        response = self.client.get(reverse('main:show_main'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'main.html')
+
+    def test_show_main_view(self): #tests if main:show_main is functioning correctly
+        response = self.client.get(reverse('main:show_main')) #simulate GET request to URL
+        self.assertEqual(response.status_code, 200) 
+        self.assertTemplateUsed(response, 'main.html') #verifies that response uses main.html
--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,5 @@ from main.views import show_main
 app_name = 'main'
 
 urlpatterns = [
-    path('', show_main, name='show_main'),
+    path('', show_main, name='show_main'), #Empty string '' means it matches the root URL, ex: http://example.com/
 ]
